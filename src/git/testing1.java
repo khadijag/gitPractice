9a59a5ad87f408bc6ed56f6d33b40eb6934012cb
Khadija Ghayur <Khadija.g2020@gmail.com>
2022-02-28 18:59:34 -0500

    this commit has all the filse

---
+++ src/git/testing1.java
@@ -0,0 +1,13 @@
+package git;
+
+public class testing1 {
+
+	public static void main(String[] args) {
+    System.out.println("she is tried"); 
+    System.out.println("she is not happy");
+		
+		
+		
+	}
+
+}
