d6feb8b381d28203743a786e444a56b8fb8fbe77
Khadija Ghayur <Khadija.g2020@gmail.com>
2022-02-28 21:30:28 -0500

    I have updateed something

--- src/git/testing1.java
+++ src/git/testing1.java
@@ -4,7 +4,7 @@ public class testing1 {
 
 	public static void main(String[] args) {
     System.out.println("she is tried"); 
-    System.out.println("she is not happy");
+    System.out.println("she is happy");
 		
 		
 		
