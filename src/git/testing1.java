6b5451dc7f8b1570e4977847909e72b27187f243
Khadija Ghayur <Khadija.g2020@gmail.com>
2022-02-28 21:50:35 -0500

    the commit that was esay

--- src/git/testing1.java
+++ src/git/testing1.java
@@ -5,7 +5,7 @@ public class testing1 {
 	public static void main(String[] args) {
     System.out.println("she is tried"); 
     System.out.println("she is happy");
-		
+    System.out.println("she is fine");
 		
 		
 	}
